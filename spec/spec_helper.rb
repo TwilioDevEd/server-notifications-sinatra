dad4a9b2e6c6389305ecf7ffc39cf5ffdb0f7d42
nebtrx <neb.trx@gmail.com>
2016-03-04 17:01:43 -0500

    Fix coding standars

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,22 +5,3 @@ require 'capybara/rspec'
 require 'capybara/dsl'
 
 Capybara.app = ServerNotifications::App
-
-
-# require 'rack/test'
-# require 'rspec'
-#
-# ENV['RACK_ENV'] = 'test'
-#
-# require File.expand_path '../../app.rb', __FILE__
-#
-# module RSpecMixin
-#   include Rack::Test::Methods
-#
-#   def app()
-#     ServerNotifications::App
-#   end
-# end
-#
-# # For RSpec 2.x and 3.x
-# RSpec.configure { |c| c.include RSpecMixin }
